b36a1e0cc285db4dc386aec1dc89233d25587160
Link4Electronics <link4electronics@gmail.com>
2026-04-08 19:12:14 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm jre17-openjdk openssl
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+VERSION=1.5
+echo "$VERSION" > ~/version
+wget https://downloadirpf.receita.fazenda.gov.br/irpf/2026/irpf/arquivos/IRPF2026-${VERSION}.zip
+bsdtar -xvf ./*.zip --strip-components=1
+rm -f ./*.zip
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+sed -i 's|\./jre/bin/java -jar irpf.jar|exec java -jar "$APPDIR/bin/irpf.jar" "$@"|' exec.sh
+mv -v exec.sh lib lib-modulos irpf.jar IRPF.acb offline.png online.png pgd-updater-1.0.0.jar ./AppDir/bin
